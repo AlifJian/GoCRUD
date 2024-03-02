cfa701b6fc7487c85857b783fbc3aa3942eaf0e5
Alif Jian <alifjian834@gmail.com>
2024-03-02 19:39:51 +0700

    Add SQL Query to create Table

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE .`user` (
+    id INT NOT NULL AUTO_INCREMENT , 
+    name VARCHAR(100) NOT NULL , 
+    email VARCHAR(100) NOT NULL ,
+    password VARCHAR(255) NOT NULL ,
+    PRIMARY KEY (`id`)
+    )
